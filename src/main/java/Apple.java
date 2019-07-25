a13c19126e39582513a8255687579c31f7a9d950
Stanislaw <kutyaev.stass@gmail.com>
2019-07-25 17:39:27 +0300

    this time we go

--- src/main/java/Apple.java
+++ src/main/java/Apple.java
@@ -1,6 +1,5 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import io.qameta.allure.Attachment;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -16,6 +15,8 @@ public class Apple {
         WebDriver driver = new ChromeDriver();
         driver.get("https://developer.apple.com/");
 
+        takeScreenshot(driver);
+
         Boolean logo = driver.findElement(By.id("ac-gn-firstfocus")).isDisplayed();
         Assert.assertTrue(logo);
 
@@ -29,6 +30,8 @@ public class Apple {
         WebDriver driver = new ChromeDriver();
         driver.get("https://developer.apple.com/");
 
+        takeScreenshot(driver);
+
         Boolean disc = driver.findElement(By.xpath("//a[text()='Discover']")).isDisplayed();
         Assert.assertTrue(disc);
 
@@ -37,8 +40,17 @@ public class Apple {
 
         WebElement title = (new WebDriverWait(driver, 2))
                 .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h1")));
+        takeScreenshot(driver);
         driver.quit();
     }
+
+    @Attachment(type = "image/png")
+    public byte[] takeScreenshot(WebDriver driver){
+        byte[] result = null;
+        if (driver != null) result = ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
+        return result;
+    }
+
 }
 
 
